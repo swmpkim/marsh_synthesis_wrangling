b38aa64285e0817fce5c90345bab06355780ff0d
Kim Cressman <kimberly.cressman@dmr.ms.gov>
2022-05-06 10:23:25 -0500

    working with GTM and NIW

---
+++ R/sourced/00_helper_functions.R
@@ -0,0 +1,93 @@
+# figure out how many rows of headers there are
+### function is modified from `check_num2` at https://stackoverflow.com/a/67532389
+### to figure out which rows can't be converted to numeric
+skip_fun <- function(x){
+    y <- suppressWarnings(as.numeric(x))
+    if(is.numeric(x)){  # if it's already a number, there was only one row of headers so that's all we need to skip (adding headers back in later)
+        return(1)
+    }
+    max(which(is.na(y))) + 1
+}
+
+check_num2 <- function(x){
+    y <- suppressWarnings(as.numeric(x))
+    which(is.na(y))
+}
+
+# read in the file, figure out how much to skip, read it in again, and rename the columns from the first test file
+read_cdmo <- function(file,
+                      worksheet = NULL,
+                      skip = NULL){
+    # worksheet option if different worksheets of interest are in the file
+    # skip option for reserves where the number to skip is known and the way to automate a guess *isn't* known  
+    to_mod <- read_xlsx(file,
+                        n_max = 10,
+                        na = c("", "NA"),
+                        sheet = worksheet)
+    
+    # which column has "Lat" in it? This should be numeric.
+    lat <- str_which(names(to_mod), "Lat")
+    lat_vec <- to_mod[[lat]]
+    
+    if(is.null(skip)){
+        skip <- skip_fun(lat_vec)  
+    }
+    
+    dat <- read_xlsx(file,
+                     sheet = worksheet,
+                     skip = skip,
+                     col_names = FALSE) 
+    names(dat) <- names(to_mod)
+    dat <- dat %>% 
+        mutate(SiteID = as.character(SiteID))
+    return(dat)
+}
+
+# same as above but turning all typed NAs into blank cells 
+read_cdmo2 <- function(file,
+                      worksheet = NULL,
+                      skip = NULL){
+    # worksheet option if different worksheets of interest are in the file
+    # skip option for reserves where the number to skip is known and the way to automate a guess *isn't* known  
+    to_mod <- read_xlsx(file,
+                        n_max = 10,
+                        na = c("", "NA"),
+                        sheet = worksheet)
+    
+    # which column has "Lat" in it? This should be numeric.
+    lat <- str_which(names(to_mod), "Lat")
+    lat_vec <- to_mod[[lat]]
+    
+    if(is.null(skip)){
+        skip <- skip_fun(lat_vec)  
+    }
+    
+    dat <- read_xlsx(file,
+                     sheet = worksheet,
+                     skip = skip,
+                     na = c("", "NA"),
+                     col_names = FALSE) 
+    names(dat) <- names(to_mod)
+    dat <- dat %>% 
+        mutate(SiteID = as.character(SiteID))
+    return(dat)
+}
+
+# bind to `all_coords`
+## don't do this twice; it doesn't check to see if these coordinates are already there
+bind_coords <- function(df){
+    to_bind <- df %>% 
+        select(Reserve, SiteID, TransectID, PlotID, Lat, Long) %>% 
+        filter(Lat != "NA") %>% 
+        mutate(Lat = str_trim(Lat),
+               Long = str_trim(Long),
+               Lat = as.numeric(Lat),
+               Long = as.numeric(Long)) %>% 
+        group_by(Reserve, SiteID, TransectID, PlotID) %>% 
+        summarize(Lat = round(mean(Lat, na.rm = TRUE), 5),
+                  Long = round(mean(Long, na.rm = TRUE), 5)) %>% 
+        ungroup() %>% 
+        select(Reserve, SiteID, Lat, Long)
+    all_coords <<- bind_rows(all_coords, to_bind)  
+    cat(paste(nrow(to_bind), "rows have been added to `all_coords`"))
+}
