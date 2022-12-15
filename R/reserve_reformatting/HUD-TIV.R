cf42d067a588367c08d0a15b4d2a2fdc033220af
Kim Cressman <kim@catbirdstats.com>
2022-12-15 13:24:51 -0600

    HUD - csv and excel files bound

--- R/reserve_reformatting/HUD-TIV.R
+++ R/reserve_reformatting/HUD-TIV.R
@@ -2,25 +2,41 @@ library(tidyverse)
 library(readxl)
 library(rgdal)
 
-# Set up path and get file names  
+# Set up path, file names, and column names  
 path_tiv <- here::here("submitted_data", "data", 
                          "HUD",
                          "HUD Tivoli Veg Data")
 
-csvs <- list.files(path_tiv, pattern = ".csv")
+csvss <- list.files(path_tiv, pattern = ".csv")
 xlsxs <- list.files(path_tiv, pattern = ".xlsx")
+
+
+col_matching <- c("Orthometric_Height" = "Orthometric Height",
+                  "Height_Relative_to_MLLW" = "Height Relative to MLLW",
+                  "Ht" = "Canopy Height",
+                  "Ht" = "Average Canopy Height",
+                  "Ht" = "Maximum Canopy Height",
+                  "Ht" = "Canopy Height (m)",
+                  "Ht" = "Canopy.Height",
+                  "Lat" = "Lat (2013)",
+                  "Long" = "Long (2013)",
+                  "Elevation" = "Elevation (2013)",
+                  "Cover" = "X..Cover",
+                  "Cover" = "% Cover")
 
 
 # pull in and bind csv files  
 csvs_in <- list()
-for(i in seq_along(csvs)){
-    csvs_in[[i]] <- read.csv(here::here(path_tiv, csvs[i]),
+for(i in seq_along(csvss)){
+    csvs_in[[i]] <- read.csv(here::here(path_tiv, csvss[i]),
                              stringsAsFactors = FALSE) %>% 
-        filter(row_number() != 1)
+        filter(row_number() != 1) %>% 
+        rename(any_of(col_matching))
 }
 
-if(janitor::compare_df_cols_same(csvs_in) == TRUE){
-    csvs <- bind_rows(csvs_in)
+if(janitor::compare_df_cols_same(csvs_in)){
+    csvs <- bind_rows(csvs_in) %>% 
+        mutate(Date = lubridate::mdy(Date))
     message("csvs bound successfully")
 } else {warning("CSVS WILL NOT BIND")}
 
@@ -31,18 +47,25 @@ for(i in seq_along(xlsxs)){
     tmp <- read_xlsx(here::here(path_tiv, xlsxs[i]),
                              sheet = "Data Entry",
                      guess_max = 2000) %>% 
-        filter(row_number() != 1)
+        filter(row_number() != 1) %>% 
+        rename(any_of(col_matching))
     names(tmp)[2] <- "Date"
 
     excels_in[[i]] <- tmp
 }
 
-if(janitor::compare_df_cols_same(excels_in) == TRUE){
-    excels <- bind_rows(excels_in)
+if(janitor::compare_df_cols_same(excels_in)){
+    excels <- bind_rows(excels_in) %>% 
+        mutate(Date = lubridate::ymd(Date))
     message("excel files bound successfully")
 } else {warning("EXCEL FILES WILL NOT BIND")}
 
 
+# bind data frames from csv and excel together
+if(janitor::compare_df_cols_same(csvs, excels)){
+    hud_tiv <- bind_rows(csvs, excels)
+    message("csv and excel data frames bound successfully")
+} else {warning("CSV AND EXCEL DATA FRAMES WILL NOT BIND")}
 
 
 # Copied from before - UTM to decimal degrees
