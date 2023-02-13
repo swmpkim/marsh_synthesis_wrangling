061c9a997381d4f495af24863b9430656dc57020
Kim Cressman <kim@catbirdstats.com>
2023-02-13 14:35:33 -0600

    checks of other worksheets

--- R/sourced/nmst-file-check-helper.R
+++ R/sourced/nmst-file-check-helper.R
@@ -32,3 +32,109 @@ if(sum(!(col_chk)) > 0){
 # check for duplicate rows  
 janitor::get_dupes(covr,
                    Year, Month, Day, SiteID, TransectID, PlotID)
+
+
+# species name checks  
+# species columns start after Notes
+spp_covr <- covr %>%
+    filter(row_number() == 1) %>% 
+    select(-(1:Notes),
+           -starts_with("F_"))
+
+# density columns from cover sheet
+spp_cvr_dens <- spp_covr %>% 
+    select(starts_with("Density")) %>% 
+    names() %>% 
+    str_remove("Density_")
+
+# height columns from cover sheet
+spp_cvr_ht <- spp_covr %>% 
+    select(contains("Height")) %>% 
+    names() 
+ht_type <- str_split(spp_cvr_ht, pattern = "_")[[1]][1]
+spp_cvr_ht <- str_remove(spp_cvr_ht, paste0(ht_type, "_"))
+# what is reported
+print(glue("Height is reported as '{ht_type}'"))
+
+# density species are in the other cover species, right?
+dens_check <- spp_cvr_dens %in% names(spp_covr)
+if(sum(!dens_check) > 0){
+    cli::cli_warn(c("Species with reported density measurements 
+                    on the main (Cover) worksheet do not have
+                    reported cover data; is something misspelled?",
+                    "x" = "{spp_cvr_dens[!dens_check]}"))
+}
+
+# and height
+ht_check <- spp_cvr_ht %in% names(spp_covr)
+if(sum(!ht_check) > 0){
+    cli::cli_warn(c("Species with reported height measurements 
+                    on the main (Cover) worksheet do not have
+                    reported cover data; is something misspelled?",
+                    "x" = "{spp_cvr_ht[!ht_check]}"))
+}
+
+
+### now from the other worksheets  
+
+
+## density
+dens <- read_xlsx(fls2[i],
+                  sheet = "Density",
+                  guess_max = 5000)
+col_chk <- id_cols %in% names(dens)
+if(sum(!(col_chk)) > 0){
+    cli::cli_warn(c("Identifying columns are missing from the Density sheet",
+                    "x" = "Missing columns are: {id_cols[!col_chk]}"))
+}
+# species names
+spp_dens <- dens %>%
+    filter(row_number() == 1) %>% 
+    select(-(1:Notes),
+           -starts_with("F_")) %>% 
+    names()
+dens_covr <- spp_dens %in% names(spp_covr)
+if(sum(!dens_covr) > 0){
+    cli::cli_warn(c("Species with reported density measurements 
+                    in the Density worksheet do not have
+                    reported cover data; is something misspelled?",
+                  "x" = "{spp_dens[!dens_covr]}"))
+}
+# dupes
+dupes_dens <- janitor::get_dupes(dens,
+                   Year, Month, Day, SiteID, TransectID, PlotID)
+if(nrow(dupes_dens) > 0){
+    cli::cli_warn("The Density sheet has duplicate rows:")
+    print(dupes_dens)
+}
+
+
+## Heights
+hts <- read_xlsx(fls2[i],
+                  sheet = "Height",
+                  guess_max = 5000)
+col_chk <- id_cols %in% names(hts)
+if(sum(!(col_chk)) > 0){
+    cli::cli_warn(c("Identifying columns are missing from the Height sheet",
+                    "x" = "Missing columns are: {id_cols[!col_chk]}"))
+}
+# species names
+spp_hts <- hts %>%
+    filter(row_number() == 1) %>% 
+    select(-(1:Notes),
+           -starts_with("F_")) %>% 
+    names()
+hts_covr <- spp_hts %in% names(spp_covr)
+if(sum(!hts_covr) > 0){
+    cli::cli_warn(c("Species with reported height measurements 
+                    in the Height worksheet do not have
+                    reported cover data; is something misspelled?",
+                    "x" = "{spp_hts[!hts_covr]}"))
+}
+# dupes
+dupes_hts <- janitor::get_dupes(hts,
+                                 Year, Month, Day, SiteID, TransectID, PlotID)
+if(nrow(dupes_hts) > 0){
+    cli::cli_warn("The Height sheet has duplicate rows:")
+    print(dupes_hts)
+}
